5c5670e54551375adb3442d0750c69268b2a9255
DESKTOP-5DLT18T\Rob Jellinghaus <rjellinghaus@live.com>
2018-03-10 21:24:47 -0800

    Simplify test case to show core issue.

--- App.cpp
+++ App.cpp
@@ -6,6 +6,7 @@
 #include <future>
 #include <sstream>
 
+using namespace concurrency;
 using namespace std::chrono;
 using namespace winrt;
 
@@ -34,10 +35,7 @@ struct App : ApplicationT<App>
     // for use inside the Click lambda?
     apartment_context _ui_thread;
 
-    int _currentTick{ 0 };
-
     TextBlock _textBlock{ nullptr };
-    Button _button1{ nullptr };
 
     void UpdateTextBlock(const std::wstring& suffix)
     {
@@ -46,53 +44,14 @@ struct App : ApplicationT<App>
         _textBlock.Text(str);
     }
 
-    // Tick forever, updating the text block at each two-second tick.
-    // Clicking the button multiple times will create multiple concurrent instances of this loop.
-    // But because of the context switching, these are effectively mutually excluded from updating
-    // the text block racefully, and these concurrent loops coexist just fine!  Each loop causes the
-    // block to be updated an additional 100x per second.
-    IAsyncAction TickForever()
-    {
-        for (;;)
-        {
-            // first, require that we are in the background while we wait
-            co_await resume_background();
-
-            // wait two seconds
-            co_await resume_after(timeSpanFromSeconds(0.01));
-
-            // switch to UI thread in order to update _textBlock.
-           co_await _ui_thread;
-
-            std::wstringstream wstr{};
-            wstr << L"Tick #" << (_currentTick++);
-            UpdateTextBlock(wstr.str());
-        }
-    }
-
 	void OnLaunched(LaunchActivatedEventArgs const&)
     {
 		_textBlock = TextBlock();
 		_textBlock.Text(AppStateString);
 
-		_button1 = Button();
-		_button1.Content(IReference<hstring>(L"Start Ticking"));
-
-		_button1.Click([&](IInspectable const&, RoutedEventArgs const&)
-		{
-            // start ticking forever
-            // QUESTION: Is this a valid way to enter an infinite asynchronous loop?  Does this need to be co_awaited?
-            // Would it cause any type of context/thread starvation if it were awaited and effectively looped forever?
-            TickForever();
-		});
-
 		Window xamlWindow = Window::Current();
 
-		StackPanel stackPanel = StackPanel();
-		stackPanel.Children().Append(_textBlock);
-		stackPanel.Children().Append(_button1);
-
-		xamlWindow.Content(stackPanel);
+		xamlWindow.Content(_textBlock);
 		xamlWindow.Activate();
 
 		// and here goes
@@ -113,16 +72,23 @@ struct App : ApplicationT<App>
         co_await resume_after(timeSpanFromSeconds(2));
 
         // Switch back and update text.
-        co_await ui_thread;
-        UpdateTextBlock(L"Initializing");
+        co_await _ui_thread;
+        UpdateTextBlock(L"Completed first wait");
 
-        // Switch away from UI thread and wait two seconds again.
-        co_await resume_background();
-        co_await resume_after(timeSpanFromSeconds(2));
+        // The lambda created here fails to properly restore its captured "this" reference after the first two co_awaits.
+        create_task([this]() -> IAsyncAction
+        {
+            // Switch away from UI thread and wait two seconds again.
+            co_await resume_background();
+            co_await resume_after(timeSpanFromSeconds(2));
 
-        // Switch back and update text again.
-        co_await ui_thread;
-        UpdateTextBlock(L"Waiting");
+            // Switch back and update text again.
+            // THIS NEXT LINE CRASHES as the value of the captured "this" pointer appears corrupt.
+            co_await _ui_thread;
+
+            // This line is not reached.
+            UpdateTextBlock(L"Completed second wait");
+        });
 	}
 };
 
